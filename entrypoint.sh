5f924315190c623c0ab94cd76f5086c2f5f0085f
Nils Balkow-Tychsen <nils.baty@humanitec.com>
2019-09-04 15:16:13 +0200

    no json

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "jsonPayload: {\"severity\": \"ERROR\", \"message\": \"An error is usually an exception that has been caught and not handled.\"}"
+      "1") echo "ERROR An error is usually an exception that has been caught and not handled"
       ;;
-      "2") echo "jsonPayload: {\"severity\": \"INFO\", \"message\": \"This is less important than debug log and is often used to provide context in the current task.\"}"
+      "2") echo "INFO This is less important than debug log and is often used to provide context in the current task."
       ;;
-      "3") echo "jsonPayload: {\"severity\": \"WARN\", \"message\": \"A warning that should be ignored is usually at this level and should be actionable.\"}"
+      "3") echo "WARN A warning that should be ignored is usually at this level and should be actionable."
       ;;
-      "4") echo "jsonPayload: {\"severity\": \"DEBUG\", \"message\": \"This is a debug log that shows a log that can be ignored.\"}"
+      "4") echo "DEBUG This is a debug log that shows a log that can be ignored."
       ;;
    esac
 done
