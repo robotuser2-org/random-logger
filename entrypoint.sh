f41a31d11588623262690ad76cdfd8cb4bc589b5
Nils Balkow-Tychsen <nils.baty@humanitec.com>
2019-09-04 15:10:53 +0200

    fiddeling with the json log payload

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "{'severity': 'ERROR', 'message': 'An error is usually an exception that has been caught and not handled.'}"
+      "1") echo "jsonPayload: {\"severity\": \"ERROR\", \"message\": \"An error is usually an exception that has been caught and not handled.\"}"
       ;;
-      "2") echo "{'severity': 'INFO', 'message': 'This is less important than debug log and is often used to provide context in the current task.'}"
+      "2") echo "jsonPayload: {\"severity\": \"INFO\", \"message\": \"This is less important than debug log and is often used to provide context in the current task.\"}"
       ;;
-      "3") echo "{'severity': 'WARN', 'message': 'A warning that should be ignored is usually at this level and should be actionable.'}"
+      "3") echo "jsonPayload: {\"severity\": \"WARN\", \"message\": \"A warning that should be ignored is usually at this level and should be actionable.\"}"
       ;;
-      "4") echo "{'severity': 'DEBUG', 'message': 'This is a debug log that shows a log that can be ignored.'}"
+      "4") echo "jsonPayload: {\"severity\": \"DEBUG\", \"message\": \"This is a debug log that shows a log that can be ignored.\"}"
       ;;
    esac
 done
